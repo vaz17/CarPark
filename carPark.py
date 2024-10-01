4183b838e5aab11c489f758eadd1b1407ddd1f40
Matthew Vazquez <mvazy17@gmail.com>
2024-10-01 11:30:09 -0400

    Minor changes

--- carPark.py
+++ carPark.py
@@ -1,31 +1,39 @@
 from pathlib import Path    #core python module
 import win32com.client      #pip install win32
 
-#output directory
-output_dir = Path.cwd() / "Output"
-output_dir.mkdir(parents=True, exist_ok=True)
+def getEmails():
+    #output directory
+    output_dir = Path.cwd() / "Output"
+    output_dir.mkdir(parents=True, exist_ok=True)
 
-#connect to outlook
-outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+    #connect to outlook
+    outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
-#connect to folder
-inbox = outlook.GetDefaultFolder(6)
+    #connect to folder
+    inbox = outlook.GetDefaultFolder(6)
 
-#get messages
-messages = inbox.Items
+    #get messages
+    messages = inbox.Items
 
-for message in messages:
-    subject = message.Subject
-    body = message.Body
-    attachments = message.Attachments
+    for message in messages:
+        subject = message.Subject
+        sender_email = message.SenderEmailAddress
+        attachments = message.Attachments
+        sent_time = message.SentOn
 
-    #create seperate target folders
-    target_folder = output_dir / str(subject)
-    target_folder.mkdir(parents=True, exist_ok=True) 
+        if (sender_email != "eastlansing@harveyec.com" and \
+            "Customizable Data Export" not in subject):
+            continue
+        else:
+            title = subject[-7:] + " " + str(sent_time)
 
-    #write body to text file
-    Path(target_folder / "EMAIL_BODY.txt").write_text(str(body))
+        # if sent_time < last_ran:
+        
 
-    #save attachemnts
-    for attachment in attachments:
-        attachment.SaveAsFile(target_folder / str(attachment))
+        #create seperate target folders
+        target_folder = output_dir / str(subject)
+        target_folder.mkdir(parents=True, exist_ok=True) 
+
+        #save attachemnts
+        for attachment in attachments:
+            attachment.SaveAsFile(target_folder / str(attachment))
